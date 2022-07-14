c66d8b4006b7ee6572171f28b949c5ec042a94f9
Mateus Coelho <mat_de_souza@hotmail.com>
2022-07-13 22:42:27 -0400

    Comments added, treatment for timeout during ping, getting and showing the best ping average results of DNS IPs List.

--- dnsverifier.py
+++ dnsverifier.py
@@ -1,18 +1,39 @@
-from msilib.schema import Media
 import os
-from statistics import mean, median
+from statistics import mean
 from ping3 import ping
-import math
 
+# Class used to manage each DNS info
+class DnsPing:
+    def __init__(self, ip, mean_resp):
+        self.ip = ip
+        self.mean_resp = mean_resp
+
+# Verify if dns.txt exists
 if os.path.exists("dns.txt"):
+
+    # Get all DNS
     with open("dns.txt", "r") as dns_file:
         dns_list = dns_file.readlines()
         dns_file.close()
-    for dns in dns_list:
+
+    dns_ping_ms = []
+
+    # Ping each DNS 10 times and get its mean response
+    for dns in dns_list:        
         pings = []
-        for cont in range (10):
-            pings.append(ping(dns.strip("\n")) * 1000)
-        
-        print("IP: {} || {:.0f} ms".format(dns.strip("\n"), mean(pings)))
+
+        if ping(dns.strip("\n")) != None and ping(dns.strip("\n")) != False: # Treatment for Timeout
+            for cont in range (10):            
+                pings.append(ping(dns.strip("\n")) * 1000)
+            
+            dns_ = DnsPing(dns.strip("\n"), mean(pings)) # Saving info in a object
+
+            dns_ping_ms.append(dns_) # Saving object for future sort
+
+        '''print("IP: {} || {:.0f} ms".format(dns.strip("\n"), mean(pings)))
         for cont in range(len(pings)):
-            print("     Ping{}: {:.0f} ms".format(cont+1, pings[cont]))
+            print("     Ping{}: {:.0f} ms".format(cont+1, pings[cont]))'''
+    
+    dns_ping_ms.sort(key=lambda x: x.mean_resp) # Sorting DNS mean response list by lowest to highest
+    for cont in range(5):
+        print("{}º - {} ({:.0f} ms)".format(cont + 1, dns_ping_ms[cont].ip, dns_ping_ms[cont].mean_resp))
